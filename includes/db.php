01bfbe206272dc42a767da7e5dba2bedfaf4dd02
Benjamin Asante <90681669+Westt89@users.noreply.github.com>
2026-08-28 17:05:40 +0000

    Update database connection to use environment variables

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'booking_system';
-$username = 'root';
-$password = '';
+$host     = getenv('DB_HOST');
+$dbname   = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
